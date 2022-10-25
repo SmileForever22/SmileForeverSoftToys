a5ce00a0aed1ee765307af00f90e822486fb807e
SmileForever22 <smileforeverst22@gmail.com>
2022-10-25 21:46:18 +0530

    second commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SmileForeverSoftToys
---
+++ frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <div className="leftFooter">
+        <h4>CONTACT US</h4>
+        <p>smileforeverst22@gmail.com</p>
+      </div>
+
+      <div className="midFooter">
+        <h1>SMILE FOREVER</h1>
+        <p>High Quality is our first priority</p>
+
+        <p>Copyrights 2022 &copy; Smile Forever</p>
+      </div>
+
+      <div className="rightFooter">
+        <h4>Follow Us</h4>
+        <a href="http://instagram.com/meabhisingh">Instagram</a>
+        <a href="http://instagram.com/meabhisingh">Facebook</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
