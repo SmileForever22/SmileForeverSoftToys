9af8a9d4e3725eaaada14c46fbb83fd689f3e616
SmilerForever22 <smileforeverst22@gmail.com>
2022-11-13 20:03:37 +0530

    03

--- backend/app.js
+++ backend/app.js
@@ -30,8 +30,8 @@ app.use('/api/v1', payment);
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
 });
 
 // Middleware for Errors
