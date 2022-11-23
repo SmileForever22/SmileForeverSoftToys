a904a9255b70b3dcc47ffdecc3588774d414f848
SmilerForever22 <smileforeverst22@gmail.com>
2022-11-23 21:25:02 +0530

    4

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,16 +7,23 @@ const crypto = require('crypto');
 const cloudinary = require('cloudinary');
 
 // Register a User
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: 'avatars',
+    width: 150,
+    crop: 'scale',
+  });
 
-const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-const user = await User.create({
-  name,
-  email,
-  password,
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  sendToken(user, 201, res);
 });
-
-sendToken(user, 201, res);
 
 // Login User
 exports.loginUser = catchAsyncErrors(async (req, res, next) => {
