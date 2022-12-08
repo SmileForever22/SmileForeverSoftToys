e8ddd67d29357e31d84e25aef2faf2c814289d6b
SmilerForever22 <smileforeverst22@gmail.com>
2022-12-08 18:59:42 +0530

    99

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
 
       <div className="rightFooter">
         <h4>Follow Us</h4>
-        <a href="http://instagram.com/meabhisingh">Instagram</a>
+        <a href="https://instagram.com/smileforever_softtoys">Instagram</a>
         <a href="http://instagram.com/meabhisingh">Facebook</a>
       </div>
     </footer>
